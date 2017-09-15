8b4f315d77d2478457f93d3953a344b141172993
Thomas Cartwright <TJCARTWRIGHT91@GMAIL.COM>
2017-09-15 20:25:00 +0100

    distance calculations

--- src/providers/location-tracker/location-tracker.ts
+++ src/providers/location-tracker/location-tracker.ts
@@ -58,10 +58,11 @@ export class LocationTrackerProvider {
 
       this.watch = this.geolocation.watchPosition(options).filter((p: any) => p.code === undefined).subscribe((position: Geoposition) => {
 
-          if(this.mapMarkers.length > 0) {
-              console.log(position.coords.latitude - this.mapMarkers[0].lat());
-          };
-
+          for(let i=0; i < this.mapMarkers.length; i++) {
+              if(this.getDistanceFromLatLonInKm(this.mapMarkers[i].lat(), this.mapMarkers[i].lng(), position.coords.latitude, position.coords.longitude)) {
+                console.log("aloha aloha");
+              }
+          }
 
           //Run update inside of Angular's zone
           this.zone.run(() => {
@@ -70,6 +71,25 @@ export class LocationTrackerProvider {
           });
 
       });
+  }
+
+
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = this.deg2rad(lon2-lon1);
+      var a =
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c; // Distance in km
+      return d;
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
   }
 
   stopTracking() {
